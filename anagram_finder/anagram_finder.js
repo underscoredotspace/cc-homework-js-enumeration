7dd744e62bda44bd6eaaed61726777e979d28b5f
Colin Tindle <ampersand@underscore.space>
2018-06-20 19:14:20 +0100

    passing tests in anagram_finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,27 @@
 const AnagramFinder = function (word) {
+  this.word = word.toLowerCase()
+}
 
+function isAnagram(base, test) {
+  if (base.length != test.length || base === test) { return false }
+
+  base = Array.from(base)
+
+  for (const letter of test) {
+    const letterIndex = base.indexOf(letter.toLowerCase())
+
+    if (letterIndex != -1) {
+      base.splice(letterIndex, 1)
+    } else {
+      return false
+    }
+  } 
+
+  return true
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  return otherWords.filter(word => isAnagram(this.word, word))
 }
 
 module.exports = AnagramFinder;
