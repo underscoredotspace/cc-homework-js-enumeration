60fdc0fe84542fc65680d8b20c846e2f834a7d80
Colin Tindle <ampersand@underscore.space>
2018-06-20 19:55:30 +0100

    added pangram

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
+  // make phrase lower case and remove non-alpha characters
+  let phrase = this.phrase.toLowerCase().replace(/[^a-z]+/g, '')
 
+  return this.alphabet.every(letter => phrase.includes(letter))
 }
 
 module.exports = PangramFinder;
