7204d452df22df38722f716511c44393b0c5ba9b
Colin Tindle <ampersand@underscore.space>
2018-06-20 19:20:41 +0100

    added upper_caser

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,9 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  return this.words.map(word => word.toUpperCase())
 }
 
 module.exports = UpperCaser;
