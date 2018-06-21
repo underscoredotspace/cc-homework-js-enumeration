e04dbee1c7c2006797aeb5e1e807c6c5846c68e4
Colin Tindle <ampersand@underscore.space>
2018-06-21 08:51:15 +0100

    added isogram

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,18 @@
 const IsogramFinder = function (word) {
-
+  this.word = Array.from(word)
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+  const letters = []
+  
+  for (const letter of this.word) {
+    if (letters.includes(letter)) {
+      return false
+    }
 
+    letters.push(letter)
+  }
+  return true
 }
 
 module.exports = IsogramFinder;
